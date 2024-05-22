fc769ed0183f1882a6dd2f62dfca9b789fb87423
greenmachine112 <jake0118green@gmail.com>
2024-05-22 16:43:35 -0700

    Expand stylesheet for terminal usage

--- src/styles.py
+++ src/styles.py
@@ -1,10 +1,20 @@
 #standard terminal output
 RESET = "\033[0m"
-#successes
+
+#successes, value changes
 GREEN = "\033[32m"
+
 #errors
 RED = "\033[31m"
+
 #return value headers
 BLUE = "\033[34m"
+
+#return value metadata
+CYAN = "\033[36m"
+
 #commands
 YELLOW = "\33[33m"
+
+#variations
+PURPLE = "\33[35m"
